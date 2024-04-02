be0e5e025c5a6be41cdf57958a3a220567445e3f
Xinzhang-Chen <xinzhang.chen@yahoo.com>
2024-04-03 00:19:37 +1100

    Upload

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/xinzhang-chen/',
+  base: '/xinzhang-chen.github.io/',
   plugins: [react()],
 });
